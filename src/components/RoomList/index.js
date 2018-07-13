ab5c15ba3f32ecef45d2fe8440897103d005706c
graberzz <mrneshta@gmail.com>
2018-07-13 20:30:30 +0500

    ADD No Rooms message

--- src/components/RoomList/index.js
+++ src/components/RoomList/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import RoomItem from '../RoomItem';
 
+const NoRooms = styled.div`
+  text-align: center;
+  color: #aaa;
+  font-size: 2em;
+`;
+
 const RoomList = ({ rooms }) => (
   <ul>
-      { rooms.map(room => 
-        <RoomItem key={room.id} room={room} />)}
+      { rooms.length ? rooms.map(room => 
+        <RoomItem key={room.id} room={room} />) :
+        <NoRooms>no rooms yet...</NoRooms> }
   </ul>
 );
 
